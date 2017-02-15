d0479660ff3286c48360790cf75bfe7177d20c78
HY M <HY M>
2017-02-15 22:56:00 +0800

    Initial commit

---
+++ p5_naive_bayes_GaussianNB/ClassifyNB.py
@@ -0,0 +1,7 @@
+def classify(features_train, labels_train, features_test):
+
+    from  sklearn.naive_bayes import GaussianNB       ### import the sklearn module for GaussianNB
+    clf = GaussianNB()                                  ### create classifier
+    clf.fit(features_train, labels_train)               ### fit the classifier on the training features and labels
+    pred = clf.predict(features_test)
+    return clf                                         ### return the fit classifier
