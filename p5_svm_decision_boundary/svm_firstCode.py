47a63aa903b340d023ce52180028655289092b1b
992635910@qq.com <qq992635910>
2017-02-18 15:15:54 +0800

    svc_practise_complete

--- p5_svm_decision_boundary/svm_firstCode.py
+++ p5_svm_decision_boundary/svm_firstCode.py
@@ -1,7 +1,7 @@
 import sys
 from class_vis import prettyPicture
 from prep_terrain_data import makeTerrainData
-
+from class_vis import prettyPicture, output_image
 import matplotlib.pyplot as plt
 import copy
 import numpy as np
@@ -14,13 +14,14 @@ features_train, labels_train, features_test, labels_test = makeTerrainData()
 ########################## SVM #################################
 ### we handle the import statement and SVC creation for you here
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(kernel="linear")#kernel="linear")
 
 
 #### now your job is to fit the classifier
 #### using the training features/labels, and to
 #### make a set of predictions on the test data
-
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
 
 #### store your predictions in a list named pred
@@ -34,3 +35,6 @@ acc = accuracy_score(pred, labels_test)
 
 def submitAccuracy():
     return acc
+
+prettyPicture(clf, labels_test, features_test)
+print(submitAccuracy())
